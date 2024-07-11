7a2ce22e1b3f9247cdac8dd359cf19b980a59c97
Grayss20 <157813957+Grayss20@users.noreply.github.com>
2024-07-11 11:26:26 +0100

    final

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-class Warior():
+class Warrior():
     def __init__(self, name, power, endurance, hair_color):
         self.name = name
         self.power = power
@@ -23,4 +23,15 @@ class Warior():
     def info(self):
         print(f'Name: {self.name}, power: {self.power}, endurance: {self.endurance},'
               f' hair_color: {self.hair_color}')
-        
+
+
+war1 = Warrior('Nik', 10, 10, 'black')
+war2 = Warrior('Egor', 20, 20, 'white')
+
+
+print(war1.power)
+war2.info()
+
+war2.sleep()
+
+war2.info()
